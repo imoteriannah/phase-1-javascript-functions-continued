664a5aa9f93d678ce6a53651dda2fffa6bf34653
imoteriannah <imoter.iannah@student.moringaschool.com>
2023-08-26 12:55:52 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  // function saturdayFun(activity) {
+  //   if (typeof activity === "undefined") activity = "roller-skate";
+  return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(spec = "*") {
+  return function (adj = "special") {
+    return `You are ${spec}${adj}${spec}!`;
+  };
+}
